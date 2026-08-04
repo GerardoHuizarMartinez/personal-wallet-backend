efedc85240b9c8d70d88b63b28d82a484215d770
GerardoHuizarMtzD <lugehuizarma@ittepic.edu.mx>
2026-08-03 22:45:44 -0700

    sustitucion de purchase por expense

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\PaymenMethodController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
@@ -27,14 +27,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-
-Route::get('/purchases', [PurchaseController::class, 'list']);
+    
+Route::get('/expenses', [ExpenseController::class, 'list']);
 Route::get('/getCategoryList', [CategoryController::class, 'list']);
 Route::get('/getCategoryListIncome', [CategoryController::class, 'listIncome']);
 Route::get('/getPaymentMethodList', [PaymenMethodController::class, 'list']);
 
-Route::post('/store-purchase-expense', [PurchaseController::class, 'store']);
-Route::delete('/delete/{expense}', [PurchaseController::class, 'destroy']);
-Route::put('/update/{id}', [PurchaseController::class, 'update']);
+Route::post('/store-expense', [ExpenseController::class, 'store']);
+Route::delete('/delete/{expense}', [ExpenseController::class, 'destroy']);
+Route::put('/update/{id}', [ExpenseController::class, 'update']);
 
 Route::get('/dashboard', [DashboardController::class, 'index']);
