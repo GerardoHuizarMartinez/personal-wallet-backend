48abc558d18f86a3f36da6ef4be4c2bb1f7d96da
GerardoHuizarMtzD <lugehuizarma@ittepic.edu.mx>
2026-08-05 22:42:38 -0700

    add user seeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -28,7 +28,7 @@ class UsersSeeder extends Seeder
             'status' => 'Active',
             'url_image' => 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.prydwen.gg%2Fzenless%2Fcharacters%2Fyixuan&psig=AOvVaw1FshCnbNKRkaLkJCLZs0bE&ust=1754031947150000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCODv-bDE5o4DFQAAAAAdAAAAABAb',
             'email' => 'gerardo-1297@hotmail.com',
-            'password' => bcrypt('12345678')
+            'password' => bcrypt('Sacrifice1?')
         ]);
     }
 }
