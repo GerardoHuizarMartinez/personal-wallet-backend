f43ebd9fc45d2526197f34f76feaa86161ef0a24
GerardoHuizarMtzD <lugehuizarma@ittepic.edu.mx>
2026-08-13 21:56:13 -0700

    fix

--- config/cors.php
+++ config/cors.php
@@ -15,13 +15,13 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'], // 👈 Agregado sanctum
 
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000','exp://192.168.100.22:8081')],
-    'allowed_origins' => ['*'],
-
+    'allowed_origins' => ['http://146.190.67.18'], // 👈 NUNCA usar '*' si supports_credentials es true
+    
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
