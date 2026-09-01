ef25f096e3e405de5585affb1262297a9ffe6e9f
Gerardo Huizar <soporte@gremti.com>
2026-08-31 23:23:01 -0700

    Add method to edit category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Services\Catalog\CategoryService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 
-class CategoryController 
+class CategoryController
 {
     public function __construct(
         private readonly CategoryService $categoryService
@@ -24,5 +25,19 @@ class CategoryController
         return response()->json(
             $this->categoryService->getIncomeCategories()
         );
+    }
+
+    public function update(int $id, Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'name'   => 'required|string|max:50',
+            'label'  => 'required|string|max:30',
+            'type'   => 'required|in:expense,income',
+            'status' => 'required|in:Active,Inactive',
+        ]);
+
+        $category = $this->categoryService->update($id, $validated);
+
+        return response()->json($category);
     }
 }
